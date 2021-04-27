d65e67ee019b754c1b8b0e254fb4a8b0cf051abc
huy <huyvo.meete@gmail.com>
2021-04-27 17:01:49 +0700

    refactor wording

--- main/main.py
+++ main/main.py
@@ -1,10 +1,10 @@
 from util.digital_signature import DigitalSignature
 
 if __name__ == '__main__':
-    signature1 = DigitalSignature(data='abc',
+    signature1 = DigitalSignature(data='This is the first content',
                                   secret='6a87c625-e186-43bc-8160-b6e80e1a910b').get_signature()
 
-    signature2 = DigitalSignature(data='abc',
+    signature2 = DigitalSignature(data='This is the second content',
                                   secret='6a87c625-e186-43bc-8160-b6e80e1a910b').get_signature()
 
     print('{}\n{}'.format(signature1, signature2))
